be85176977924d9806403ff87b9791cc67d46513
Francososa <franky.sosa@gmail.com>
2018-09-18 18:03:27 -0600

    Implement sieve of eratosthenes recursively

--- src/Primes.hs
+++ src/Primes.hs
@@ -2,5 +2,10 @@ module Primes
     ( primes
     ) where
 
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter (not . (== 0) . (`mod` nextPrime)) rest
+
 primes :: [Int]
-primes = [1 .. ]
+primes = sieve [2 .. 10000]
