66628e337f5d12d3ee59478f5f702e039a6afe9b
Francososa <franky.sosa@gmail.com>
2018-09-19 19:27:06 -0600

    Refactor tests to work with Either

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,39 +1,45 @@
-import Data.Maybe
+import Primes.Types
+import Data.Either
 import Test.QuickCheck
 import Primes
 
 
-prop_validPrimesOnly val = if val < 2 || val >= length primes
-                             then result == Nothing
-                             else isJust result
+--prop_validPrimesOnlyOUT val = if val < 2 || val >= length primes
+--                             then result == PrimeError
+--                             else isRight result
+--  where result = isPrime val
+
+prop_validPrimesOnly val
+  | val < 2 = isLeft result
+  | val >= length primes = isLeft result
+  | otherwise = isRight result
   where result = isPrime val
 
-
-prop_primesArePrime val = if result == Just True
+prop_primesArePrime val = if fromRight False result
                             then length divisors == 0
                             else True
   where result = isPrime val
         divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
 
 
-prop_nonPrimesAreComposite val = if result == Just False
+prop_nonPrimesAreComposite val = if fromRight True result == False
                                     then length divisors > 0
                                     else True
   where result = isPrime val
         divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
         
 
-prop_factorsMakeOriginal val = if result == Nothing
+prop_factorsMakeOriginal val = if isLeft result 
                                   then True
-                                  else product (fromJust result) == val
+                                  else product (fromRight [0] result) == val
   where result = primeFactors val
 
 
-prop_allFactorsPrime val = if result == Nothing
+prop_allFactorsPrime val = if isLeft result
                              then True
-                             else all (== Just True) resultsPrime
+                             else all (isRight) resultsPrime
   where result = primeFactors val
-        resultsPrime = map isPrime (fromJust result)
+        resultsPrime = map isPrime (fromRight [0] result)
         
 
 main :: IO ()
